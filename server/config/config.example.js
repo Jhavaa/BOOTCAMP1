538745a5cff9c5487dfcef97525ae54c460562f0
Jhanava Ramisetty <Jramisetty@ufl.edu>
2019-09-29 19:29:40 -0400

    Config.example.js updated

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   db: {
-    uri: 'mongodb+srv://User:<password>@cluster0-8xt7l.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+    uri: 'mongodb+srv://User:8vde9mKGJPRq7f0E@cluster0-8xt7l.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
   }, 
   openCage: {
     key: '4956de4148b84f3286d875546dc60611' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
