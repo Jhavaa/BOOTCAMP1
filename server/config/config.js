d1dd38aab7409db89ca7da4d691e93d553b950a1
Jhanava Ramisetty <Jramisetty@ufl.edu>
2019-10-23 14:35:22 -0400

    fresh

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://User:Avjwip0gQ9DPFLCn@cluster0-8xt7l.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
-  }, 
-  openCage: {
-    key: '4956de4148b84f3286d875546dc60611' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
-  },
-  port: 8080
-};
